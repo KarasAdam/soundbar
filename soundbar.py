91db474f88b7e80b01816bf1c5db5cce19d3e6a2
Adam <adamos.ak33@gmail.com>
2026-03-07 19:05:17 +0100

    added funcionality with cirtual cable

--- soundbar.py
+++ soundbar.py
@@ -4,12 +4,15 @@ import keyboard
 import sounddevice as sd
 import soundfile as sf
 import numpy as np
-
+import winsound
+print(sd.query_devices())
 #1. key reckognition
 
 def fart1():
+    print(f"działa")
     data, sample = sf.read('fart1.wav')
-    sd.play(data, sample)
+    sd.play(data, sample, device=12)
+    winsound.PlaySound('fart1.wav', winsound.SND_FILENAME | winsound.SND_ASYNC)
 
 keyboard.add_hotkey(" ", fart1)
 
